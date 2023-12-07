50bf4fad14679060f2504436f0dc5dc210d4bd1a
Iloniavo <hei.iloniavo@gmail.com>
2023-12-07 17:02:26 +0300

    feat: check parameters

--- functions/app.py
+++ functions/app.py
@@ -1,16 +1,32 @@
 import json
+import os
 
 # import requests
 
 
 def lambda_handler(event, context):
-    query_params = event.get('queryStringParameters', {})
-    a = int(query_params.get('a', 0))
-    b = int(query_params.get('b', 0))
-    result = a + b
+    query_params = event['queryStringParameters']
+    zoom_size = query_params.get('zoom_size')
+    folder_name = query_params.get('folder_name')
+    geojson = json.loads(event['body'])
+    server_path = "server\lyon_wms.json"
+
+    # Change directory /tmp
+    os.chdir('/tmp')
+    
+   # Create a new .geojson file
+    with open('temp.geojson', 'w') as f:
+       json.dump(geojson, f)
+
+   # Wait until the file is created
+    while not os.path.exists('temp.geojson'):
+       time.sleep(1)
+
+    new_geojson_path = "tmp\temp.geojson"
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result" : result
+            "test" : new_geojson_path
         })
     }
