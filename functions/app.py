bd027488c5c15c66ebd1c0b5536fb51bec32ea45
NyAndoMayah <hei.mayah.3@gmail.com>
2023-12-07 16:39:49 +0300

    chore: call get_geo_tiles in app.py

--- functions/app.py
+++ functions/app.py
@@ -1,16 +1,14 @@
 import json
+import shutil
+from geo_tiles_download import get_geo_tiles
 
 # import requests
 
 
 def lambda_handler(event, context):
     query_params = event.get('queryStringParameters', {})
-    a = int(query_params.get('a', 0))
-    b = int(query_params.get('b', 0))
-    result = a + b
+    geo_tiles_download(server, '/tmp/geo_tiles/', force, tiles=None, zoom=None, bbox=None, geojson=None)
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "result" : result
-        })
+        "body": "zipped geo tiles created successfully"
     }
