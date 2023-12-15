9e39648738d92de603356b642460c1c4dff99a8c
NyAndoMayah <hei.mayah.3@gmail.com>
2023-12-15 13:41:24 +0300

    chore: lambda handles two files

--- functions/app.py
+++ functions/app.py
@@ -4,6 +4,10 @@ import io
 import os
 import time
 import base64
+import cgi
+from email import message_from_string
+from io import BytesIO
+from multipart import parse_form_data
 from geo_tiles_download import get_geo_tiles
 from flask import Flask, send_file
 from export_zip import export_zip
@@ -23,20 +27,30 @@ def encoded_payload_from_event(event):
     else:
         return payload.encode("utf-8")
 
+
 def lambda_handler(event, context):
-        server_path = "server/lyon_wms.json"
         query_params = event.get("queryStringParameters", {})
         zoom_size = query_params.get('zoom_size')
 
-        with open('/tmp/temp.geojson', 'w') as f:
-            json.dump(encoded_payload_from_event(event).decode(), f)
+        fp = io.BytesIO(event['body'].encode('utf-8'))
+        pdict = cgi.parse_header(event['headers']['Content-Type'])[1]
+        if 'boundary' in pdict:
+            pdict['boundary'] = pdict['boundary'].encode('utf-8')
+        pdict['CONTENT-LENGTH'] = len(event['body'])
+        form_data = cgi.parse_multipart(fp, pdict)
+
+        geojson = form_data.get('geojson')[0]
+        server = form_data.get('server')[0]
+
+        with open('/tmp/temp.geojson', 'w') as json_file:
+            json.dump( json.loads(geojson.decode('utf-8')), json_file)
+        with open('/tmp/server.json', 'w') as json_file:
+            json.dump(json.loads(server.decode('utf-8')), json_file)
+
         folder = '/tmp/geo_tiles'
 
-        while not os.path.exists('/tmp/temp.geojson'):
-            time.sleep(1)
+        get_geo_tiles("/tmp/server.json", folder, True, tiles=None, zoom=[zoom_size], bbox=None, geojson=['/tmp/temp.geojson'])
 
-        get_geo_tiles(server=server_path, output=folder, force=True, tiles=None, zoom=[zoom_size], bbox=None, geojson=['/tmp/temp.geojson'])
-        
         return {
             "statusCode": 200,
             "body": to_base64(export_zip(folder)),
@@ -45,6 +59,4 @@ def lambda_handler(event, context):
                 'Content-Disposition': 'attachment; filename="files.zip"'
             },
             "isBase64Encoded": True
-        } 
-
-       
+        }
